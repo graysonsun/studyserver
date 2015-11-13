6ffd4634c53066103a729b0dbc4423fa67c020da
graysonsun <sungc.orient@gmail.com>
2015-11-13 17:32:57 +0800

    follow a demo, but it cant run now

--- poll/app.js
+++ poll/app.js
@@ -22,8 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// app.use('/', routes);
+// app.use('/users', users);
+//app.get('/', routes.index);
+app.get('/polls/polls', routes.list);
+app.get('/polls/:id', routes.poll);
+app.post('/polls', routes.create);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ poll/public/javascripts/services.js
@@ -0,0 +1,12 @@
+    angular.module('pollServices', ['ngResource']).
+    factory('Poll', function($resource) {
+        return $resource('polls/:pollId', {}, {
+            query: {
+                method: 'GET',
+                params: {
+                    pollId: 'polls'
+                },
+                isArray: true
+            }
+        })
+    });
